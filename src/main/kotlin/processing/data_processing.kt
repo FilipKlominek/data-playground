7d470ff9488611eb1a3a25f0f8a41c81a7dd714b
FilipKlominek <filip.klominek@educanet.cz>
2021-12-09 22:13:37 +0100

    commit

--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -1,42 +1,63 @@
 package processing
 
-import streams.*
+import streams.Gender
+import streams.Grade
+import streams.GradeType
+import streams.Student
+import java.util.*
+import java.util.function.Consumer
+import kotlin.collections.ArrayList
 
 
 fun atLeastOneGradeA(student: Student): Boolean {
-    TODO()
+    val grades = student.grades
+    val gradeAs = grades.filter { grade -> grade.type == GradeType.A }
+    return gradeAs.isNotEmpty()
 }
 
 
 fun getStudentAges(students: List<Student>): List<Int> {
-    TODO()
+    val ages = ArrayList<Int>()
+    students.forEach(Consumer { student: Student -> ages.add(student.age) })
+    return ages
 }
 
 fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
-    TODO()
+    return students.filter { student -> student.age >= minAge }
 }
 
 
 // gender == Gender.MALE
 // or gender.name == "MALE"
 fun countMaleStudents(students: List<Student>): Int {
-    TODO()
+    return students.filter { student -> student.gender == Gender.MALE }.size
 }
 
 
 // gender == Gender.FEMALE
 // or gender.name == "FEMALE"
 fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    TODO()
+    val femaleStudents = students.filter { student -> student.gender == Gender.FEMALE }
+    val ages = femaleStudents.map { femaleStudent -> femaleStudent.age }
+    val sum = ages.fold(0) { acc, i -> acc + i }
+    return (sum / ages.size).toDouble()
 }
 
 fun getProductOfStudentAges(students: List<Student>): Int {
-    TODO()
+    val ages = ArrayList<Int>()
+    students.forEach { student -> ages.add(student.age) }
+    var product = 1
+    ages.forEach { age -> product *= age }
+    return product
 }
 
 // ignore F Grades
 fun productOfStudentGrades(student: Student): Int {
-    TODO()
+    val grades = ArrayList<Int>()
+    student.grades.forEach { grade -> grades.add(grade.type.value) }
+    var product = 1
+    grades.forEach { grade -> product *= grade }
+    return product
 }
 
 // region BONUS
